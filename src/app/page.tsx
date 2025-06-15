7d7f51ba8ba3cfa1f093bdeb61138dedf1f999d1
Rohit-Ghising <ghisingrohit48@gmail.com>
2025-06-15 23:09:09 +0545

    Signin Form Done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,10 @@ import { authClient } from "@/lib/auth-client";
 
 
 export default function Home()  {
+  const { 
+        data: session, 
+        //refetch the session
+    } = authClient.useSession() 
    const [email ,setEmail] = useState("");
    const [name ,setName] = useState("");
    const [password ,setPassword] = useState("");
@@ -19,12 +23,37 @@ export default function Home()  {
      onSuccess:()=>{window.alert('Success')}
 
   },
+
   
    
   
   )}
-  return (
-    <div className="p-4 flex flex-col gap-y-4">
+  // ----------------------
+   const onLogin = ()=>{
+    
+    authClient.signIn.email({
+    email,password
+   },
+  {
+    onError:()=>{window.alert("someyhomng went wrong")},
+     onSuccess:()=>{window.alert('Success')}
+
+  },
+
+  
+   
+  
+  )}
+  if (session){
+    return (
+      <div className="flex flex-col p-4 gap-y-4 ">
+        <p>Logged in as { session.user.name}</p>
+        <Button onClick={()=> authClient.signOut()}>Sign Out</Button>
+      </div>
+    )
+  }
+  return ( <div className="flex flex-col gap-y-10 ">
+     <div className="p-4 flex flex-col gap-y-4">
       <Input placeholder="name" value={name} onChange={(e)=>setName(e.target.value)}/>
         <Input placeholder="email" value={email} onChange={(e)=>setEmail(e.target.value)}/>
           <Input placeholder="password" type="password" value={password} onChange={(e)=>setPassword(e.target.value)}/>
@@ -33,5 +62,17 @@ export default function Home()  {
           </Button>
     
     </div>
+    {/* //--------- */}
+     <div className="p-4 flex flex-col gap-y-4">
+      
+        <Input placeholder="email" value={email} onChange={(e)=>setEmail(e.target.value)}/>
+          <Input placeholder="password" type="password" value={password} onChange={(e)=>setPassword(e.target.value)}/>
+          <Button onClick={onLogin}>
+            Login
+          </Button>
+    
+    </div>
+  </div>
+   
   )
 }
