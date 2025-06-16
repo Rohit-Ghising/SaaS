655483c37e3e5e3d61b30310eb2676195b4a5bfd
Rohit-Ghising <ghisingrohit48@gmail.com>
2025-06-16 12:19:23 +0545

    form valadiation done

--- src/modules/auth/ui/views/sign-in-view.tsx
+++ src/modules/auth/ui/views/sign-in-view.tsx
@@ -6,9 +6,13 @@ import {Input} from '@/components/ui/input'
 import {Button} from '@/components/ui/button'
 import {Form,FormControl,FormField,FormItem,FormLabel,FormMessage} from '@/components/ui/form'
 import {Alert,AlertTitle} from "@/components/ui/alert"
+import { authClient } from '@/lib/auth-client';
 
 import { Card, CardContent } from "@/components/ui/card"
 import { useForm } from 'react-hook-form';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 const formSchema = z.object({
   email:z.string().email(),
   password:z.string().min(1,{message:"Password is required"})
@@ -16,6 +20,9 @@ const formSchema = z.object({
 
 
 export const SignInView= ()=>{
+  const router = useRouter()
+  const [error,setError] = useState<string|null>(null)
+  const [pending,setPending] = useState<boolean>()
   const form = useForm<z.infer<typeof formSchema>>({
     resolver:zodResolver(formSchema),
     defaultValues:{
@@ -23,13 +30,32 @@ export const SignInView= ()=>{
       password:""
     }
   })
+  const onSubmit = (data:z.infer<typeof formSchema>)=>{
+    setError(null);
+    setPending(true)
+     authClient.signIn.email(
+      {
+        email:data.email,
+        password:data.password
+      },
+      {
+        onSuccess: ()=>{
+          router.push('/')
+          setPending(false)
+        },
+        onError:({error})=>{
+          setError(error.message)
+        }
+      },
+    )
+  }
 
   return(
     <div className="flex flex-col gap-6">
       <Card className="overflow-hidden p-0 ">
  <CardContent className="grid p-0 md:grid-cols-2">
 <Form {...form}>
-    <form className='p-6 md:p-8'><div className='flex flex-col gap-6 '>
+    <form className='p-6 md:p-8' onSubmit={form.handleSubmit(onSubmit)}><div className='flex flex-col gap-6 '>
       <div className='flex flex-col items-center text-center '>
         <h1 className='text-2xl font-bold '>Welcome Back!</h1> 
         <p className='text-muted-foreground text-balance'>
@@ -83,24 +109,45 @@ export const SignInView= ()=>{
   )}
         />
       </div>
-      {true &&(
+      {!!error &&(
         <Alert className='bg-destructive/10 border-none'>
           <OctagonAlertIcon className='h-4 w-4 !text-destructive'/>
           <AlertTitle >
-            Error
+            {error}
           </AlertTitle>
         </Alert>
       )}
-      <Button type='submit' className='w-full'>
+      <Button type='submit' className='w-full' disabled={pending}>
         Sign In
       </Button>
       {/* -- */}
-      <Button className='after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after: z-0 after:flex after:items-center after:border-t'>
+      <div className='after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after: z-0 after:flex after:items-center after:border-t'>
       <span className='bg-card text-muted-foreground relative z-10 px-2'>
         Or Continue with
       </span>
 
-      </Button>
+      </div>
+      {/* -------------------------------------- */}
+      <div className='grid grid-cols-2 gap-4'>
+        <Button variant='outline'type='button'
+        className='w-full'>
+          Google
+
+        </Button>
+        <Button variant='outline'type='button'
+        className='w-full'>
+          Gitub
+
+        </Button>
+      </div>
+      {/* ------ */}
+      <div className=' text-center text-sm '>Don't have an account?{" "}
+        <Link href='/sign-up' className='underline underline-offset-4'>
+        Sign Up
+        </Link>
+
+      </div>
+
       </div>
       </form>
 </Form>
@@ -109,7 +156,12 @@ export const SignInView= ()=>{
     <p className="text-2xl font-semibold text-white">Meet.AI</p>
   </div>
  </CardContent>
-      </Card></div>
+      </Card>
+      
+      <div className='text-muted-foreground *:[a]:hover:text-primary text-center text-xs  text-balance *:[a]:underline *:[a]:underline-offset-4'>
+        By clicking , you are agree to our <a href="#">Terms of Service </a>and <a href="#">Privacy Policy</a>
+      </div>
+      </div>
     
   )
 }
